399133998e697a3cb97270cbf8fd37bb874579cf
Junaid Ali <mailtojunaid@gmail.com>
2019-03-23 17:59:31 +0000

    added outputs and variables files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,3 @@
-# variables
-variable "container_name" {
-  description = "name of container"
-  default = "blog"
-}
-
-variable "image" {
-  description = "image for container"
-  default = "ghost:alpine"
-}
-
-variable "int_port" {
-  description = "internal port for container"
-  default = "2368"
-}
-
-variable "ext_port" {
-  description = "external port for container"
-  default = "80"
-}
-
 # download the ghost image
 resource "docker_image" "image_id" {
   name = "${var.image}"
@@ -32,13 +11,4 @@ resource "docker_container" "container_id" {
      internal = "${var.int_port}"
      external = "${var.ext_port}"
   }
-}
-
-# add output
-output "IP Address" {
-  value = "${docker_container.container_id.ip_address}"
-}
-
-output "container_name" {
-  value = "${docker_container.container_id.name}"
 }
