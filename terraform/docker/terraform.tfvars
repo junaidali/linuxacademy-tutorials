27751c603b4a134de73fdc6605670b3e7da3886e
Junaid Ali <mailtojunaid@gmail.com>
2019-03-23 19:14:53 +0000

    added tfvars file

---
+++ terraform/docker/terraform.tfvars
@@ -0,0 +1,14 @@
+image = {
+  dev = "ghost:latest"
+  prod =  "ghost:alpine"
+}
+
+container_name = {
+  dev = "dev_blog"
+  prod = "prod_blog"
+}
+
+ext_port = {
+  dev = 8080
+  prod = 80
+}
--- terraform/docker/variables.tf
+++ terraform/docker/variables.tf
@@ -6,20 +6,12 @@ variable "env" {
 variable "image" {
   description = "container image"
   type = "map"
-  default = { 
-     "dev" = "ghost:latest"
-     "prod" =  "ghost:alpine"
-  }
 }
 
 # container variables
 variable "container_name" {
   description = "name of container"
   type = "map"
-  default = {
-     "dev" = "dev_blog"
-     "prod" =  "prod_blog"
-  }
 }
 
 variable "int_port" {
@@ -30,8 +22,4 @@ variable "int_port" {
 variable "ext_port" {
   description = "external port for container"
   type = "map"
-  default = {
-     "dev" = "8080"
-     "prod" =  "80"
-  }
 }
